e7f30fe555810d43c9e5b8b5220cd185bf475294
Nicholas <nich.otieno@gmail.com>
2019-07-12 10:58:14 +0300

    Bug fix
    Signed-off-by: Nicholas <nich.otieno@gmail.com>

--- app/src/main/java/tk/crucial/alc4phase1/MyProfileActivity.java
+++ app/src/main/java/tk/crucial/alc4phase1/MyProfileActivity.java
@@ -2,11 +2,8 @@ package tk.crucial.alc4phase1;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -23,14 +20,6 @@ public class MyProfileActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         ImageView profilePhoto = findViewById(R.id.imageview_profile_photo);
